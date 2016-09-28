278756bd842f77243c1ad07c8fe6e9b87d98db02
Andrew Toups <mistertoups@gmail.com>
2016-09-27 22:19:22 -0400

    completed formatting and sending data to document

--- js/main.js
+++ js/main.js
@@ -1,100 +1,75 @@
-function wordFrequency (string) {
-  //create stats object
-  var stats = {};
-  // console.log("raw string:");
-  // console.log(string);
+function wordFrequency(string) {
+    //create stats object
+    var stats = {};
 
-  //make lowercase
-  string = string.toLowerCase();
+    //make lowercase
+    string = string.toLowerCase();
 
-  //separate punctuation
-  string = string.replace(/\n/g, '');
-  string = string.replace(/,/g, ' , ');
-  string = string.replace(/:/g, ' : ');
-  string = string.replace(/;/g, ' ; ');
-  string = string.replace(/"/g, ' " ');
-  string = string.replace(/'/g, " ' ");
-  string = string.replace(/-/g, ' - ');
-  string = string.replace(/\./g, ' . ');
-  // console.log("after replacing stuff: ");
-  // console.log(string);
+    //separate punctuation
+    string = string.replace(/\n/g, '');
+    string = string.replace(/,/g, ' , ');
+    string = string.replace(/:/g, ' : ');
+    string = string.replace(/;/g, ' ; ');
+    string = string.replace(/"/g, ' " ');
+    string = string.replace(/'/g, " ' ");
+    string = string.replace(/-/g, ' - ');
+    string = string.replace(/\./g, ' . ');
 
-  // break string into array of words:
-  words = string.split(' ');
-  console.log("after split: ");
-  console.log(words);
+    // break string into array of words:
+    words = string.split(' ');
 
-  // create object for each array item and increment value each time:
-  // create words array index:
-  var index = words.length - 1;
-  // initialize stats object with first words array item:
-  // stats[words[index]] = 0;
-  // console.log("intialized stats object:");
-  // console.log(stats);
-  while (index >= 0){
-    var currentString = words[index]
-    if (currentString.length > 1){       //itemize stats object
-      stats[currentString] = 0;
-    }//add keyname from array string
-    index--;
-  }
+    // create words array index:
+    // indexing from the end of array bc idk ¯\_(ツ)_/¯
+    var index = words.length - 1;
 
-  console.log("itemized stats object:");
-  console.log(stats);
-  index = words.length; //count words
-  while (index >= 0){
-    for (var keyname in stats){
-      if (words[index] === keyname) { // word match
-        stats[words[index]]++;        //add keyname from array string
-      }
+    // initialize stats object with first words array item:
+    while (index >= 0) {
+        var currentString = words[index]
+        if (currentString.length > 1) { //itemize stats object
+            stats[currentString] = 0;
+        } //add keyname from array string
+        index--;
     }
-    index--;
-  }
-  console.log("counted stats object:");
-  console.log(stats);
-  var node;
-  for (word in stats) {
-    // currentStat = word + " | " + stats[word];
-    node = document.createTextNode(word);
-    span.appendChild(node);
-    statsElement.appendChild(span);
-    node = document.createTextNode(" | ");
-    span.appendChild(node);
-    statsElement.appendChild(span);
-    node = document.createTextNode(stats[word]);
-    span.appendChild(node);
-    statsElement.appendChild(span);
-  }
 
+    index = words.length; //count words
+    while (index >= 0) {
+        for (var keyname in stats) {
+            if (words[index] === keyname) { // word match
+                stats[words[index]]++; //add keyname from array string
+            }
+        }
+        index--;
+    }
 
-
-
-
-
-
-
-
-
-  // display(string, 'sonnet-stats');
+    var node;
+    for (word in stats) { // put stats on screen
+        // probably should've set functions for this up but ¯\_(ツ)_/¯
+        var para = document.createElement('p');
+        statsElement.appendChild(para);
+        var span1 = document.createElement('span');
+        para.appendChild(span1);
+        span1.className = 'info';
+        span1.innerHTML = word;
+        var span2 = document.createElement('span');
+        para.appendChild(span2);
+        span2.className = 'info';
+        span2.innerHTML = ' | ';
+        var span3 = document.createElement('span');
+        para.appendChild(span3);
+        span3.className = 'info';
+        span3.innerHTML = stats[word];
+    }
+    //more manual styling to adjust for stat content:
+    document.getElementById('sonnet-stats').style.height = 'auto';
+    document.getElementById('sonnet').style.height = document.getElementById('sonnet-stats').offsetHeight + 'px';
+    return stats;
 }
 
 
 
-
-
-
-
-
-function display(value, target) {
-  document.getElementById(target).innerHTML = value;
-}
-
-
-
-
-
-
-
+//HACKY GARBAGE DOWN HERE
+// | | | |
+// V V V V
 
 
 // set up button to grab sonnet from document
@@ -103,18 +78,10 @@ var btn = document.getElementById('calculate');
 var sonnet = document.getElementById('sonnet').innerHTML;
 var statsElement = document.getElementById('sonnet-stats');
 btn.addEventListener('click', function() {
-  wordFrequency(sonnet);
+    wordFrequency(sonnet);
 });
 
-var span = document.createElement("span");
-var para = document.createElement("p");
-// var node = document.createTextNode("This is new.");
-// para.appendChild(node);
-//
-//
-// statsElement.appendChild(para);
-
-// manually style stuff bc I'm a jerk:
+// manually style stuff bc ugh css:
 
 boxHeight = document.getElementById('sonnet').offsetHeight;
 document.getElementById('sonnet-stats').style.height = boxHeight + 'px';
